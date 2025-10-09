f8490f3c334b9a77c1f073e53744ec655673a6e0
Divine Oke <divine.oke@pau.edu.ng>
2025-10-09 13:13:32 +0100

    (Divine's 2nd Project)

--- week 2/Project_2.rs
+++ week 2/Project_2.rs
@@ -1,21 +1,18 @@
 fn main() {
-	let t1:f64 = 450_000.00;
-	let t2:f64 = 450_000.00;
-	let m1:f64 = 1_500_000.00;
-    let h1:f64 = 750_000.00;      // variables given to the sales record
-    let h2:f64 = 750_000.00;
-    let h3:f64 = 750_000.00;
-    let d1:f64 = 2_850_000.00;
-    let d2:f64 = 2_850_000.00;
-    let d3:f64 = 2_850_000.00;
-    let a1:f64 = 250_000.00;
+	let t:f64 = 2.0 * 450000.0;
+	let m:f64 = 1500000.0;
+    let h:f64 = 3.0 * 750000.0;      // variables given to the sales record
+    let d:f64 = 3.0 * 2850000.0;
+    let a:f64 = 250000.0;
+    let n:f64 = 2.0 + 1.0 + 3.0 + 3.0 + 1.0;
 
 	// sum of the sales record
 
-	let sum = t1 + t2 + m1 + h1 + h2 + h3 + d1 + d2 + d3 + a1
+	let sum = t + m + h + d + a;
 	println!("The sum of the following sales record is {}", sum);
      
      // average of the sales record
-	let average = sum / 10;
+	let average = sum / n;
 	println!("The average of the following sales record is {}", average);
+
 }
