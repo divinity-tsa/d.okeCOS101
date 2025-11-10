278d84e217d129bee623c25deb86adeb8bb8f2a0
Divine Oke <divine.oke@pau.edu.ng>
2025-11-10 17:29:48 +0100

    (Week-7 practice)

--- week-7/practice-9/src/main.rs
+++ week-7/practice-9/src/main.rs
@@ -1,4 +1,4 @@
-    // The iter() function fetches values of all elements in an array
+    // The iter() function fetches values of all elementss in an array
     fn main(){
 
         let arr:[i32;4] = [10,20,30,40];
