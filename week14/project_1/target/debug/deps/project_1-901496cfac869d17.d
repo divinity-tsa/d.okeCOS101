b597f4ec9d2a5ddd3fd4fbb1156885fe6b7239f1
Divine Oke <divine.oke@pau.edu.ng>
2026-01-18 19:53:18 +0100

    Week-14 project

---
+++ week14/project_1/target/debug/deps/project_1-901496cfac869d17.d
@@ -0,0 +1,5 @@
+c:\Users\New\Documents\d.okeCOS101\week14\project_1\target\debug\deps\project_1-901496cfac869d17.d: src\main.rs
+
+c:\Users\New\Documents\d.okeCOS101\week14\project_1\target\debug\deps\libproject_1-901496cfac869d17.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week14/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\d.okeCOS101\week14\project_1\target\debug\project_1.exe: C:\Users\New\Documents\d.okeCOS101\week14\project_1\src\main.rs
